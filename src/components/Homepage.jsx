0314853a84b8d2066dbda7ec3fb61e7d3ee76c7d
TshepoMadira <madiratshepo936@gmail.com>
2024-10-14 15:08:21 +0200

    Dynamic Check-Out Min Date

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./homepage.css";
 import Footer from "./footer";
+import BookingForm from "./Bookingform"; 
 
 const Homepage = () => {
   return (
@@ -19,29 +20,32 @@ const Homepage = () => {
         </div>
       </header>
 
+           
+        <BookingForm /> 
+
       <div className="container">
         <div className="room-offers">
-          <h2>Room Offers</h2>
-            <div className="room-card">
-              <h3>King Suite</h3>
-              <img
-                src="src/assets/images/Fitted Wardrobe Design Ideas for a Clutter Free Bedroom.jpeg"
-                alt="King Suite"
-                className="room-image"
-              />
-              <p className="room-price">R5000</p>
-              <div className='room-description-container'>
-                <p className="room-description">
-                  -Two bathrooms with double sink vanity<br />
-                  -Flat screen TV<br />
-                  -Full size sofa with lounge chairs<br />
-                  -Free Uncapped wifi<br />
-                  -Private Balcony
-                </p>
-              </div>
-            </div>
           
-          <div className="room-card">
+          <div className="room-container">
+            <h3>King Suite</h3>
+            <img
+              src="src/assets/images/Bernhardt Maxime King Upholstered Wing Panel Bed in Cream _ Wood _ Nebraska Furniture Mart.jpeg"
+              alt="King Suite"
+              className="room-image"
+            />
+            <p className="room-price">R5000</p>
+            <div className='room-description'>
+              <p className="description">
+                - Two bathrooms with double sink vanity<br />
+                - Flat screen TV<br />
+                - Full size sofa with lounge chairs<br />
+                - Free Uncapped wifi<br />
+                - Private Balcony
+              </p>
+            </div>
+          </div>
+          
+          <div className="room-container">
             <h3>Queen Suite</h3>
             <img
               src="src/assets/images/Queen Size Lift Up Storage Bed Frame Upholstered Platform Bed with Gas Lift up Hydraulic Storage and Gold Stripes Headboard.jpeg"
@@ -49,16 +53,16 @@ const Homepage = () => {
               className="room-image"
             />
             <p className="room-price">R5100</p>
-            <div className='room-description-container'>
-              <p className="room-description">
-                -Queen size bed with plus pillows and high-quality linens<br />
-                -Spacious bathroom with separate shower and bathtub<br />
-                -Flatscreen TV with premium channels
+            <div className='room-description'>
+              <p className="description">
+                - Queen size bed with plush pillows and high-quality linens<br />
+                - Spacious bathroom with separate shower and bathtub<br />
+                - Flatscreen TV with premium channels
               </p>
             </div>
           </div>
         
-          <div className="room-card">
+          <div className="room-container">
             <h3>Presidential Suite</h3>
             <img
               src="src/assets/images/Design firm Gettys and The Peninsula Hong Kong redefine the luxury travel experience.jpeg"
@@ -66,8 +70,8 @@ const Homepage = () => {
               className="room-image"
             />
             <p className="room-price">R2500</p>
-            <div className="room-description-container">
-              <p className="room-description">
+            <div className="room-description">
+              <p className="description">
                 - Comfortable bed<br />
                 - Single vanity with high-end fixtures<br />
                 - Complimentary breakfast<br />
@@ -77,9 +81,10 @@ const Homepage = () => {
             </div>
           </div>
         </div>
-        </div>
         
 
+
+      </div>
 
       <Footer />
     </div>
